e3be3f75a25e0fb3b348d36dd4c5799eec4c7494
BlackFire <apoorvagarg9148@gmail.com>
2021-08-28 19:19:13 +0530

    home tak ka backend complete...go for its front end

--- app/src/main/java/com/example/peerpowerclub/MainActivity.java
+++ app/src/main/java/com/example/peerpowerclub/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.peerpowerclub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(2*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),Login.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
     }
 }
---
+++ app/src/main/java/com/example/peerpowerclub/user.java
@@ -0,0 +1,19 @@
+package com.example.peerpowerclub;
+
+public class user {
+
+    public String fullname, email,phonenumber, areaOfInterest, prefferedTime;
+    public user()
+    {
+
+    }
+    public user(String fullname, String email, String phonenumber, String areaOfInterest, String prefferedTime)
+    {
+        this.fullname = fullname;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.areaOfInterest = areaOfInterest;
+        this.prefferedTime = prefferedTime;
+
+    }
+}
