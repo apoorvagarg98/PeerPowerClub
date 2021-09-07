b6a3d3e7c6a6e40e834723c3385cf8d0f09bcc50
HP <durjoybarua31@gmail.com>
2021-09-08 03:53:42 +0600

    login and Registration

---
+++ app/src/main/java/com/example/peerpowerclub/feed.java
@@ -0,0 +1,14 @@
+package com.example.peerpowerclub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
