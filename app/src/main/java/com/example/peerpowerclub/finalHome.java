a71c842893223acf9edf7f265168f54f1d0a3431
BlackFire <apoorvagarg9148@gmail.com>
2021-09-07 23:38:48 +0530

    yo

---
+++ app/src/main/java/com/example/peerpowerclub/finalHome.java
@@ -0,0 +1,21 @@
+package com.example.peerpowerclub;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class finalHome extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_final_home,container,false);
+        return view;
+
+    }
+}
--- app/src/main/java/com/example/peerpowerclub/myHome.java
+++ app/src/main/java/com/example/peerpowerclub/myHome.java
@@ -4,9 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,15 +24,35 @@ public class myHome extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(onNav);
-getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new Fragment1()).commit();
+getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new finalHome()).commit();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.profile:
+              getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new Fragment1()).commit();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private BottomNavigationView.OnNavigationItemSelectedListener onNav = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment selected = null;
             switch (item.getItemId()){
                 case R.id.profile_bottom:
-                    selected = new Fragment1();
+                    selected = new finalHome();
                     break;
                 case R.id.chat_bottom:
                 selected = new Fragment2Chat();
