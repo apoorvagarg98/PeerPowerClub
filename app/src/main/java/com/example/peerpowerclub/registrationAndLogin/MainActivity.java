bd602a630940832f3351f7f04e9e9501579dc323
BlackFire <apoorvagarg9148@gmail.com>
2021-09-14 21:51:08 +0530

    done

--- app/src/main/java/com/example/peerpowerclub/registrationAndLogin/MainActivity.java
+++ app/src/main/java/com/example/peerpowerclub/registrationAndLogin/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.peerpowerclub.R;
+import com.example.peerpowerclub.strt1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,23 +15,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Thread background = new Thread() {
-            public void run() {
-                try {
-                    // Thread will sleep for 5 seconds
-                    sleep(2*1000);
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
 
-                    // After 5 seconds redirect to another intent
-                    Intent i=new Intent(getBaseContext(), startactivity.class);
-                    startActivity(i);
+        if (isFirstRun) {
+            //show start activity
 
-                    //Remove activity
-                    finish();
-                } catch (Exception e) {
+            startActivity(new Intent(MainActivity.this, strt1.class));
+            Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG)
+                    .show();
+        }
+        else{
+            Thread background = new Thread() {
+                public void run() {
+                    try {
+                        // Thread will sleep for 5 seconds
+                        sleep(2*1000);
+
+                        // After 5 seconds redirect to another intent
+                        Intent i=new Intent(getBaseContext(), startactivity.class);
+                        startActivity(i);
+
+                        //Remove activity
+                        finish();
+                    } catch (Exception e) {
+                    }
                 }
-            }
-        };
-        // start thread
-        background.start();
+            };
+            // start thread
+            background.start();
+
+        }
+
+
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                .putBoolean("isFirstRun", false).commit();
+
     }
 }
--- app/src/main/java/com/example/peerpowerclub/strt2.java
+++ app/src/main/java/com/example/peerpowerclub/strt2.java
@@ -1,14 +1,60 @@
 package com.example.peerpowerclub;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
 public class strt2 extends AppCompatActivity {
+    private GestureDetectorCompat gestureDetectorCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_strt2);
+        gestureDetectorCompat = new GestureDetectorCompat(this, new MyGestureListener());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureDetectorCompat.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        //handle 'swipe left' action only
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+
+         /*
+         Toast.makeText(getBaseContext(),
+          event1.toString() + "\n\n" +event2.toString(),
+          Toast.LENGTH_SHORT).show();
+         */
+
+            if(event2.getX() < event1.getX()){
+                Toast.makeText(getBaseContext(),
+                        "Swipe left - startActivity()",
+                        Toast.LENGTH_SHORT).show();
+
+                //switch another activity
+                Intent intent = new Intent(
+                       strt2.this, strt3.class);
+                startActivity(intent);
+            }
+
+            return true;
+        }
+
+
     }
 }
