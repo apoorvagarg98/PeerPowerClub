7895e37c7812259b3b86db2621501759c0ebd613
HP <durjoybarua31@gmail.com>
2021-10-05 02:44:48 +0600

    login and Registration

---
+++ app/src/main/java/com/example/peerpowerclub/joinchat.java
@@ -0,0 +1,14 @@
+package com.example.peerpowerclub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class joinchat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joinchat);
+    }
+}
